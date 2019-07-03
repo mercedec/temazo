97ea73faa99bc9cc0c9ce5edf4187ed09362bd84
mercedec <merceicas@gmail.com>
2019-07-03 09:43:38 +0200

    Add Jordi code

--- temazo.sh
+++ temazo.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
 #        https://www.youtube.com/results?search_query=thunderstruck+seagulls
-
+#DEBUG set -x
 quebusco=$(echo $*  | tr " " "+" )
 urlachupar=$(curl -s "https://www.youtube.com/results?search_query=$quebusco"| grep -m1 "/watch" | sed "sx.*href=\"/watchxhttps://www.youtube.com/watchx" | cut -d \" -f1)
-
-FILENAME="$(youtube-dl -x --audio-format mp3 --audio-quality 0 "$urlachupar"  |& fgrep '[ffmpeg] Destination: ' | sed "s/^\[ffmpeg\] Destination: //" )"
+#DEBUG /youtube-dl -x --audio-format mp3 --audio-quality 0 "$urlachupar" 
+FILENAME="$(/youtube-dl -x --audio-format mp3 --audio-quality 0 "$urlachupar"  |& fgrep '[ffmpeg] Destination: ' | sed "s/^\[ffmpeg\] Destination: //" )"
 echo $FILENAME
 NEWFILENAME="$( echo $FILENAME | sed "s/-[^-]\+\.mp3/.mp3/" )"
-mv "$FILENAME" "$NEWFILENAME"
-mplayer "$NEWFILENAME"
+#mkdir /music
+mv "$FILENAME" "/music/$NEWFILENAME"
+
